241e352e025ed7b187b5c352e1e88a477eefe069
nickambrose7 <nickambrose7@gmail.com>
2023-04-29 11:16:12 -0700

    check in for Pierce

--- src/api/conversations.py
+++ src/api/conversations.py
@@ -39,35 +39,6 @@ async def add_conversation(movie_id: int, conversation: ConversationJson):
 
     The endpoint returns the id of the resulting conversation that was created.
     """
-
-    # # ensure that all characters are part of the referenced movie:
-    # if not db.characters[conversation.character_1_id].movie_id == movie_id:
-    #     raise HTTPException(status_code=403, detail="Character 1 not in movie")
-    # if not db.characters[conversation.character_2_id].movie_id == movie_id:
-    #     raise HTTPException(status_code=403, detail="Character 2 not in movie")
-    # # ensure that the characters are not the same:
-    # if conversation.character_1_id == conversation.character_2_id:
-    #     raise HTTPException(status_code=403, detail="Characters are the same")
-    # # ensure that the lines of a conversation match the characters involved in the conversation:
-    # for line in conversation.lines:
-    #     if line.character_id != conversation.character_1_id and line.character_id != conversation.character_2_id:
-    #         raise HTTPException(status_code=403, detail="Line does not match characters")
-    # # create and add conversation:
-    # db.last_convo_id += 1
-    # convo = {"conversation_id": db.last_convo_id, "character1_id": conversation.character_1_id, "character2_id": conversation.character_2_id, "movie_id": movie_id}
-    # db.add_new_convo(convo)
-    
-    # #create and add lines:
-    # lines = []
-    # line_sort = 0
-    # for line in conversation.lines:
-    #     db.last_line_id += 1
-    #     line_sort += 1
-    #     lines.append({"line_id": db.last_line_id, 
-    #                   "character_id": line.character_id, "movie_id": movie_id, 
-    #                   "conversation_id": db.last_convo_id, "line_sort": line_sort, 
-    #                   "line_text": line.line_text})
-    # db.add_new_lines(lines)
     character_ids = {conversation.character_1_id, conversation.character_2_id}
     if len(character_ids) != 2:
         raise HTTPException(status_code=400, detail="Characters must be different")
